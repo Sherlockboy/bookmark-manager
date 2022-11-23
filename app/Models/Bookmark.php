a1b85cefe325720a876c774fa7df00ed1eb52bb6
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2022-11-23 19:51:54 +0500

    Create bookmarks

---
+++ app/Models/Bookmark.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Bookmark extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'url',
+        'description',
+        'user_id'
+    ];
+    
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(
+            related: User::class,
+            foreignKey: 'user_id',
+            ownerKey: 'id',
+        );
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -41,4 +44,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function bookmarks(): HasMany
+    {
+        return $this->hasMany(
+            related: Bookmark::class,
+            foreignKey: 'user_id',
+            localKey: 'id',
+        );
+    }
 }
